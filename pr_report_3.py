9949578ca915881554554fcce2bce61afecac734
Sean Chiou <stchiou@yahoo.com>
2019-03-15 15:43:56 -0400

    added user input prompt. CSV file was read into an array.

--- pr_report_3.py
+++ pr_report_3.py
@@ -1,6 +1,10 @@
 # Declare arrays to store raw data from CSV Files
+WeekNumber=input("Please Enter Week Number: ")
+CutOff=input("Please enter cutoff date in the format of mm/dd/yyyy: ")
 import csv
-f=open("C:\Users\chious\Box Sync\vba-projects\pr-status\week0\v3-test-data-03-13-2019.csv")
-csv_f=csv.reader(f)
-for row in csv_f:
-    print row
+with open(r'''C:\Users\chious\Box Sync\vba-projects\pr-status\week0\v3-test-data-03-13-2019.csv''') as csvfile:
+    readCSV=csv.reader(csvfile, delimiter=",")
+    rawdata=[]
+    headers=next(readCSV)
+    for row in readCSV:
+        rawdata.append(row)
