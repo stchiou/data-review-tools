e49e1c9b93e10fc54960bd4faefbd284d9fb5a66
Sean Chiou <stchiou@yahoo.com>
2019-03-15 14:47:55 -0400

    read csv file

--- pr_report_3.py
+++ pr_report_3.py
@@ -1,2 +1,6 @@
 # Declare arrays to store raw data from CSV Files
-raw_data=[]
+import csv
+f=open("C:\Users\chious\Box Sync\vba-projects\pr-status\week0\v3-test-data-03-13-2019.csv")
+csv_f=csv.reader(f)
+for row in csv_f:
+    print row
